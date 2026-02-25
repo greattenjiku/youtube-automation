8b27ec5e65468a8768b1128807ddd630890747f9
greattenjiku <greattenjiku1947@gmail.com>
2026-02-25 08:49:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+NICHE = "AI news, geopolitics, technology, India news"
 
+VIDEO_FORMAT = "YouTube Shorts"
+VIDEO_DURATION = "30 seconds"
+
+SCRIPT_STYLE = "curiosity driven, shocking, fast paced"
+
+OUTPUT_FOLDER = "output"
